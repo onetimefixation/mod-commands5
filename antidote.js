bbfd4bf4b1bf8283711983948cec21b4b6ccef65
onetimefixation <onetimefixation@gmail.com>
2022-09-05 20:15:53 -0400

    added antidote command 090522

---
+++ antidote.js
@@ -0,0 +1,13 @@
+
+// VERSION
+//  -Initial 060321
+
+
+
+export function antidote(client, tags, channel, param_message){
+
+
+
+        client.say(channel, `Check out Rinoa's NEW YouTube channel with mostly unedited calls: https://www.youtube.com/c/RinoaAntidote`);
+        
+    }
